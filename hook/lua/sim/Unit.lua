2b5cad4ebf6a87c04b9e48cb1f2c89554f005fbe
speed2 <GTGTGT@seznam.cz>
2019-04-12 19:54:57 +0200

    Use buffs from bps instead of global ones.
    This includes buffing weapons.

---
+++ hook/lua/sim/Unit.lua
@@ -0,0 +1,28 @@
+local oldUnit = Unit
+
+Unit = Class(oldUnit){
+    -- Use the veterancy buffs from bp instead of global ones, buffing weapons as well.
+    SetVeteranLevel = function(self, level)
+        -- Inform all weapons that have veteraned
+        local old = self.Sync.VeteranLevel - 1
+        for i = 1, self:GetWeaponCount() do
+            local wep = self:GetWeapon(i)
+            wep:OnVeteranLevel(old, level)
+        end
+
+        -- Check for unit buffs
+        local bp = self:GetBlueprint().Buffs
+        if bp then
+            local lvlkey = 'VeteranLevel' .. level
+            for k, v in bp do
+                if v.Add[lvlkey] == true then
+                    self:AddBuff(v)
+                end
+            end
+        end
+
+        -- And do normal callbacks
+        self:GetAIBrain():OnBrainUnitVeterancyLevel(self, level)
+        self:DoUnitCallbacks('OnVeteran')
+    end,
+}
